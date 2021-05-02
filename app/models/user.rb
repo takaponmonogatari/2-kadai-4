7f488933ad3a77c1d92b1c7319edc72d393dd604
takaponmonogatari <taka11hiro26@gmail.com>
2021-05-02 09:03:16 +0000

    [Update] kadai3

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
