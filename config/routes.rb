4dd8fcedf42cb3c69686ee6feb7f86c4fea4ee96
takaponmonogatari <taka11hiro26@gmail.com>
2021-05-03 22:44:10 +0000

    [Update] kadai4

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
   resources :users,only: [:show,:index,:edit,:update]
+  resources :rerationships, only: [:create, :destroy]
   resources :books do
     resource :favorites, only: [:create, :destroy]
 
